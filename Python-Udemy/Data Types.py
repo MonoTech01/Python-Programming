638afd4c4f9a5499eb1e7e49c190cc690718f0cb
MonoTech <159852435+MonoTech01@users.noreply.github.com>
2024-05-29 19:23:57 -0700

    Create Data Types.py

---
+++ Python-Udemy/Data Types.py
@@ -0,0 +1,9 @@
+two_digit_number = input("Type a two digit number: ")
+
+# way1
+# first_digit= two_digit_number[0]
+# second_digit= two_digit_number[1]
+# result =int(two_digit_number[0])+int(two_digit_number[1])
+# print(result)
+
+print(int(two_digit_number[0]) + int((two_digit_number[1])))
