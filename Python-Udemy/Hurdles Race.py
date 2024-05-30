d2dd70b09019033e73119ceae5fa63d5566f9b98
MonoTech <159852435+MonoTech01@users.noreply.github.com>
2024-05-29 18:55:16 -0700

    Create Hurdles Race.py

---
+++ Python-Udemy/Hurdles Race.py
@@ -0,0 +1,37 @@
+#Access the following website to play the game:
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+i=0   
+while i<6:
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    i+=1
+
+# Another way
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump_set():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# for e in range(6):
+#     jump_set()
+    
